28a0c7980b3d16a5c9270eecd413d7bf8736e000
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-09-20 11:51:36 +0300

    done tasks

--- src/3.ts
+++ src/3.ts
@@ -7,6 +7,17 @@
   Інтерфейс ISpellCaster повинен включати метод castSpell.
 */
 
+interface ICharacter {
+  name: string;
+  level: number;
+  introduce(phrase: string): void;
+  levelUp(): void;
+}
+
+interface ISpellCaster{
+  castSpell(): void;
+}
+
 // реалізація класу Wizard
 class Wizard implements ICharacter, ISpellCaster {
   constructor(public name: string, public level: number) {
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,43 @@
+class Key {
+  private signature: number = Math.random();
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+	protected door: boolean = false;
+	protected key: Key;
+	protected tenants: Person[] = [];
+	abstract openDoor(key: Key): void;
+	comeIn(tenant: Person): void {
+		if (this.door) {
+			this.tenants.push(tenant);
+		}
+	}
+}
+
+class MyHouse extends House {
+	constructor(key: Key) {
+		super();
+		this.key = key;
+	}
+	openDoor(key: Key): void {
+		if (this.key.getSignature() === key.getSignature()) this.door = true;
+	}
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
